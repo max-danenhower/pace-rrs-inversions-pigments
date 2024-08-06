b9550963628174c51bdf4a0c39d6af9e85382da7
Max Danenhower <mdanenhower@Maxs-MacBook-Pro.local>
2024-08-06 16:54:31 -0700

    adding header

--- rrs_inversion_pigments.py
+++ rrs_inversion_pigments.py
@@ -5,6 +5,11 @@ from typing import Tuple, Union
 import numpy as np
 from scipy.optimize import least_squares
 
+'''
+Originally developed in MatLab by Ali Chase and can be found here: https://github.com/alisonpchase/Rrs_inversion_pigments
+Translated to Python by Max Danenhower.
+'''
+
 G1 = 0.0949  # g1 and g2 are values from Gordon et al., 1988
 G2 = 0.0794
 LNOT = 400  # reference lambda wavelength (nm)
